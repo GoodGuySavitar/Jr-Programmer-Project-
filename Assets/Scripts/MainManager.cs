e1fc95aabb4bc82589c0f1d06d7e44bf00ed4e7d
Asmit Singh Chauhan <blr.asmit@gmail.com>
2024-02-01 23:53:05 +0530

    OOP
    -Applied OOP principles in previously written code

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -6,8 +6,7 @@ using UnityEngine;
 
 public class MainManager : MonoBehaviour
 {
-    public static MainManager Instance;
-
+    public static MainManager Instance { get; private set;}  
     public Color TeamColor;
     private void Awake(){
         if(Instance !=null){
